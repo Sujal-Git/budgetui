5961933b6ebc7572560e80a3c25ff299df12ecce
Sujal_Git <75317379+Sujal-Git@users.noreply.github.com>
2025-06-30 16:36:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {UserProvider} from './context/UserContext';
-import {ExpenseProvider} from './context/ExpenseContext';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import Dashboard from './components/Dashboard/Dashboard';
-import Landing from './components/Landing/Landing';
 
-function App() {
-  return (
-    <UserProvider>
       <ExpenseProvider>
       <Router>
       <Routes>
